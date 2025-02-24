e90f0a4fd5e83ec9f40f57d01b5ed8d0acca60f9
Tuan Tran <tuan.tran@genefriendway.com>
2025-02-24 16:12:19 +0700

    bet

--- go/main.go
+++ go/main.go
@@ -1,26 +1,7 @@
 package main
 
-import (
-	"fmt"
+import "fmt"
 
-	"github.com/tuantran-genetica/human-network-crypto-lib/pkg/pre/mocks"
-)
-
-// Example usage function
 func main() {
-	// Save a new keypair
-	err := mocks.SaveKeyPairToFile("test_keypair.json")
-	if err != nil {
-		fmt.Printf("Failed to save keypair: %v\n", err)
-		return
-	}
-
-	// Load the keypair
-	_, err = mocks.LoadKeyPairFromFile("test_keypair.json")
-	if err != nil {
-		fmt.Printf("Failed to load keypair: %v\n", err)
-		return
-	}
-
-	fmt.Println("Successfully loaded keypair!")
+	fmt.Println("Hello world")
 }
